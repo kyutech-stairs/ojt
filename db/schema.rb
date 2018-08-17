54190d6623503e2bfb91abe97f59696f20b4f780
RyosukeYoshii <kakkakka09@gmail.com>
2018-08-17 13:39:36 +0900

    update crew page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_28_125343) do
+ActiveRecord::Schema.define(version: 2018_08_16_172653) do
 
   create_table "checklists", force: :cascade do |t|
     t.integer "number"
